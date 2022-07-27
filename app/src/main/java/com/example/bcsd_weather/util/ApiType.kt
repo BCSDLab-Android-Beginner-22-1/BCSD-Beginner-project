35f03adc36f9bc495d7424c074f387a974a6b8bf
kongwoojin <kongwoojin03@gmail.com>
2022-07-28 00:53:30 +0900

    Add utils

---
+++ app/src/main/java/com/example/bcsd_weather/util/ApiType.kt
@@ -0,0 +1,11 @@
+package com.example.bcsd_weather.util
+
+/**
+ * NOW_CAST: 초단기실황조회
+ * SHORT_FORECAST: 초단기예보조회
+ * FORECAST: 단기예보조회
+ */
+
+enum class ApiType {
+    NOW_CAST, SHORT_FORECAST, FORECAST
+}
