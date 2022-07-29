9d65ff9bc0b62117b4194c90e8216bed73c707ff
kongwoojin <kongwoojin03@gmail.com>
2022-07-29 11:57:25 +0900

    Set input to nullable

--- app/src/main/java/com/example/bcsd_weather/util/OpenSettings.kt
+++ app/src/main/java/com/example/bcsd_weather/util/OpenSettings.kt
@@ -6,7 +6,7 @@ import android.net.Uri
 import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContract
 
-class OpenSettings : ActivityResultContract<Unit, Int>() {
+class OpenSettings : ActivityResultContract<Unit?, Int>() {
     override fun createIntent(context: Context, input: Unit?): Intent {
         val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
         val uri: Uri = Uri.fromParts("package", context.packageName, null)
