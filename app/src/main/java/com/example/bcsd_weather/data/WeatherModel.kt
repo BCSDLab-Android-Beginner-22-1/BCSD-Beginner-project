6c2e080fff13473e0c2d6e2f88ef848ead4f0962
KimJunsu <55648193+junsu1023@users.noreply.github.com>
2022-07-30 17:23:15 +0900

    Add data class

---
+++ app/src/main/java/com/example/bcsd_weather/data/WeatherModel.kt
@@ -0,0 +1,21 @@
+package com.example.bcsd_weather.data
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherModel(
+    @SerializedName("TMP") var temp: String = "",            // 온도
+    @SerializedName("PCP") var precipitation: String = "",   // 강수량
+    @SerializedName("PEH") var humidity: String = "",        // 습도
+    @SerializedName("WSP") var windSpeed: String = "",       // 풍속
+    @SerializedName("fcstTime") var fcstTime: String = "",   // 예보시각
+)
+
+// xml 파일 형식을 data class로 구현
+data class WEATHER(val response: RESPONSE)
+data class RESPONSE(val header: HEADER, val body: BODY)
+data class HEADER(val resultCode: Int, val resultMsg: String)
+data class BODY(val dataType: String, val items: ITEMS, val totalCount: Int)
+data class ITEMS(val item: List<ITEM>)
+
+// category: 자료 구분, fcstDate: 예측 날짜, fcstTime: 예측 시간, fcstValue = 예보 값
+data class ITEM(val category: String, val fcstDate: String, val fcstTime: String, val fcstValue: String)
