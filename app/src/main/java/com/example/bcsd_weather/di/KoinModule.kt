d27e2fbe5d60563034c42e175a7cc3195da4b2a6
kongwoojin <kongwoojin03@gmail.com>
2022-08-08 22:23:52 +0900

    Add GPSRemoteDataSource to KoinModule

--- app/src/main/java/com/example/bcsd_weather/di/KoinModule.kt
+++ app/src/main/java/com/example/bcsd_weather/di/KoinModule.kt
@@ -1,13 +1,18 @@
 package com.example.bcsd_weather.di
 
 import com.example.bcsd_weather.data.repository.GPSRepositoryImpl
+import com.example.bcsd_weather.data.repository.remote.GPSRemoteDataSource
+import com.example.bcsd_weather.data.repository.remote.GPSRemoteDataSourceImpl
 import com.example.bcsd_weather.domain.repository.GPSRepository
 import com.example.bcsd_weather.domain.usecase.GetGPSLocation
 import com.example.bcsd_weather.domain.usecase.InitGPSLocation
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
     single<GPSRepository> { GPSRepositoryImpl(get()) }
+
+    single<GPSRemoteDataSource> { GPSRemoteDataSourceImpl(androidContext()) }
 
     single { GetGPSLocation(get()) }
     single { InitGPSLocation(get()) }
