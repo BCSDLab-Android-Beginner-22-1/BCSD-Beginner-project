48a573037ea59ec9dace0f50398e37a1ad21d073
kongwoojin <kongwoojin03@gmail.com>
2022-08-12 18:23:27 +0900

    Remove api url from BuildConfig

--- data/src/main/java/com/example/bcsd_weather/data/api/RetrofitBuilder.kt
+++ data/src/main/java/com/example/bcsd_weather/data/api/RetrofitBuilder.kt
@@ -1,11 +1,10 @@
 package com.example.bcsd_weather.data.api
 
-import com.example.bcsd_weather.data.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
-    private const val baseUrl = BuildConfig.Weather_Api_Address
+    private const val baseUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/"
 
     private fun getRetrofit(): Retrofit =
         Retrofit.Builder()
