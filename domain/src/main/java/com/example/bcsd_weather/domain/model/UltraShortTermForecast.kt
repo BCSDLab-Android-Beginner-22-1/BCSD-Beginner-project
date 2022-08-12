0aad60845ca210a7f80203088b45b142902fe63a
kongwoojin <kongwoojin03@gmail.com>
2022-08-12 20:04:17 +0900

    Remove unused value from UltraShortTermForecast

--- domain/src/main/java/com/example/bcsd_weather/domain/model/UltraShortTermForecast.kt
+++ domain/src/main/java/com/example/bcsd_weather/domain/model/UltraShortTermForecast.kt
@@ -2,11 +2,5 @@ package com.example.bcsd_weather.domain.model
 
 data class UltraShortTermForecast(
     var temperature: String? = null,
-    var skyState: String? = null,
-    var precipitationTypes: String? = null,
-    var precipitation: String? = null,
-    var humidity: String? = null,
-    var windDirection: String? = null,
-    var windSpeed: String? = null,
     var forecastTime: String? = null
 )
