6435b12f10b80acf02a85d420606b934dd5c96f3
kongwoojin <kongwoojin03@gmail.com>
2022-08-14 22:47:34 +0900

    Use PrimaryKey instead of primaryKeys

--- data/src/main/java/com/example/bcsd_weather/data/model/CurrentWeatherEntity.kt
+++ data/src/main/java/com/example/bcsd_weather/data/model/CurrentWeatherEntity.kt
@@ -2,14 +2,16 @@ package com.example.bcsd_weather.data
 
 import androidx.room.Entity
 import androidx.room.Index
+import androidx.room.PrimaryKey
 
-@Entity(tableName = "current_weather", indices = [Index(value = ["fcstTime"])], primaryKeys = ["fcstTime"])
+@Entity(tableName = "current_weather", indices = [Index(value = ["fcstTime"])])
 data class CurrentWeatherEntity(
+    @PrimaryKey
+    var fcstTime: String = "",        // 예보시각
     var temp: String = "",            // 온도
     var precipitation: String = "",   // 강수량
     var humidity: String = "",        // 습도
-    var windSpeed: String = "",       // 풍속
-    var fcstTime: String = "",        // 예보시각
+    var windSpeed: String = ""       // 풍속
 )
 
 
