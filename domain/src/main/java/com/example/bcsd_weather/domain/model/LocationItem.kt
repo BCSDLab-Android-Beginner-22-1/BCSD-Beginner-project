e0a7a72bc8c9893c862b2d24f39f4f9307107099
kongwoojin <kongwoojin03@gmail.com>
2022-08-15 20:52:17 +0900

    Update LocationItem

--- domain/src/main/java/com/example/bcsd_weather/domain/model/LocationItem.kt
+++ domain/src/main/java/com/example/bcsd_weather/domain/model/LocationItem.kt
@@ -2,6 +2,6 @@ package com.example.bcsd_weather.domain.model
 
 data class LocationItem(
     val title: String?,
-    val latitude: String?,
-    val longitude: String?
+    val x: Int?,
+    val y: Int?
 )
