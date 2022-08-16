5d026262c6eb0f8864849cb67d1a9adc2792311f
kongwoojin <kongwoojin03@gmail.com>
2022-08-16 17:34:38 +0900

    Cast date as integer

--- data/src/main/java/com/example/bcsd_weather/data/dao/FutureWeatherDao.kt
+++ data/src/main/java/com/example/bcsd_weather/data/dao/FutureWeatherDao.kt
@@ -13,13 +13,13 @@ interface FutureWeatherDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertFutureWeather(futureWeatherEntity: FutureWeatherEntity)
 
-    @Query("select * from future_weather where date(fcstDate) >= date(:startDate)")
+    @Query("select * from future_weather where CAST(fcstDate AS INTEGER) >= CAST(:startDate AS INTEGER)")
     fun getFutureWeather(startDate: String): List<FutureWeatherEntity>
 
-    @Query("select * from future_weather where date(fcstDate) = date(:date)")
+    @Query("select * from future_weather where fcstDate = :date")
     fun getDetailedFutureWeather(date: String): FutureWeatherEntity
 
-    @Query("delete from future_weather where date(fcstDate) < date(:firstDateToKeep)")
+    @Query("delete from future_weather where CAST(fcstDate AS INTEGER) < CAST(:firstDateToKeep AS INTEGER)")
     fun deleteWeather(firstDateToKeep: String)
 
 }
